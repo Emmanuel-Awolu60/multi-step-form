da865f2d61180d2c6713ba87d874aa310570a57b
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-11-04 21:00:23 +0100

    updated

---
+++ starter/main.js
@@ -0,0 +1,17 @@
+const nameValue = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const next = document.getElementById("next");
+const value = document.querySelectorAll("input");
+const first = document.querySelector("#first");
+const second = document.querySelector("#second");
+
+// next.addEventListener("click", () => {
+//   const con = phone && email && nameValue.value === "";
+//   if (con) {
+//     alert("input something");
+//   } else {
+//     first.classList.add("hidden");
+//     second.classList.remove("hidden");
+//   }
+// });
