dc93a78b5fe67e5e0c919b04b53a4f16b31ace7e
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2024-01-15 21:19:58 +0100

    updated

--- starter/main.js
+++ starter/main.js
@@ -11,7 +11,7 @@ function handleform(event, idname) {
   for (let i = 0; i < allBtn.length; i++) {
     allBtn[i].className = allBtn[i].className.replace(" active", "");
   }
-  document.getElementById(idname).style.display = "flex";
+  document.getElementById(idname).style.display = "block";
   event.currentTarget.className += " active";
 }
 
